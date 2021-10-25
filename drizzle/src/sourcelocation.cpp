4c0745a0f8bf6c705a83b05c81e1344cd5cf4198
jsmolka <jsmolka@pm.me>
2021-10-25 20:51:40 +0200

    formatting

--- drizzle/src/sourcelocation.cpp
+++ drizzle/src/sourcelocation.cpp
@@ -1,10 +1,16 @@
 #include "sourcelocation.h"
 
 SourceLocation::SourceLocation(const char* position)
-    : type(Type::Position), position(position) {}
+    : type(Type::Position), position(position) 
+{
+}
 
 SourceLocation::SourceLocation(std::string_view position)
-    : SourceLocation(position.data()) {}
+    : SourceLocation(position.data())
+{
+}
 
 SourceLocation::SourceLocation(std::size_t line)
-    : type(Type::Line), line(line) {}
+    : type(Type::Line), line(line)
+{
+}
