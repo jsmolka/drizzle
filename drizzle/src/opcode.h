e0191e77a2cdd230f64e945bdec38de901ea2979
jsmolka <jsmolka@pm.me>
2022-01-29 11:07:38 +0100

    add opcode formatter

--- drizzle/src/opcode.h
+++ drizzle/src/opcode.h
@@ -2,6 +2,8 @@
 
 #include <limits>
 
+#include <sh/fmt.h>
+
 #include "int.h"
 
 enum class Opcode {
@@ -50,3 +52,60 @@ enum class Opcode {
 };
 
 static_assert(int(Opcode::LastEnumValue) <= std::numeric_limits<u8>::max());
+
+template<>
+struct fmt::formatter<Opcode> : fmt::formatter<std::string_view> {
+  template<typename FormatContext>
+  auto format(const Opcode& opcode, FormatContext& ctx) const {
+    auto repr = [](const Opcode& opcode) {
+      static_assert(int(Opcode::LastEnumValue) == 41);
+      switch (opcode) {
+        case Opcode::Add:               return "Add";
+        case Opcode::BitwiseAnd:        return "BitwiseAnd";
+        case Opcode::BitwiseAsr:        return "BitwiseAsr";
+        case Opcode::BitwiseComplement: return "BitwiseComplement";
+        case Opcode::BitwiseLsl:        return "BitwiseLsl";
+        case Opcode::BitwiseLsr:        return "BitwiseLsr";
+        case Opcode::BitwiseOr:         return "BitwiseOr";
+        case Opcode::BitwiseXor:        return "BitwiseXor";
+        case Opcode::Call:              return "Call";
+        case Opcode::Constant:          return "Constant";
+        case Opcode::ConstantExt:       return "ConstantExt";
+        case Opcode::Divide:            return "Divide";
+        case Opcode::DivideInt:         return "DivideInt";
+        case Opcode::Equal:             return "Equal";
+        case Opcode::False:             return "False";
+        case Opcode::Greater:           return "Greater";
+        case Opcode::GreaterEqual:      return "GreaterEqual";
+        case Opcode::Jump:              return "Jump";
+        case Opcode::JumpFalse:         return "JumpFalse";
+        case Opcode::JumpFalsePop:      return "JumpFalsePop";
+        case Opcode::JumpTrue:          return "JumpTrue";
+        case Opcode::Less:              return "Less";
+        case Opcode::LessEqual:         return "LessEqual";
+        case Opcode::LoadVariable:      return "LoadVariable";
+        case Opcode::LoadVariableExt:   return "LoadVariableExt";
+        case Opcode::Modulo:            return "Modulo";
+        case Opcode::Multiply:          return "Multiply";
+        case Opcode::Negate:            return "Negate";
+        case Opcode::Not:               return "Not";
+        case Opcode::NotEqual:          return "NotEqual";
+        case Opcode::Null:              return "Null";
+        case Opcode::Pop:               return "Pop";
+        case Opcode::PopMultiple:       return "PopMultiple";
+        case Opcode::PopMultipleExt:    return "PopMultipleExt";
+        case Opcode::Power:             return "Power";
+        case Opcode::Print:             return "Print";
+        case Opcode::Return:            return "Return";
+        case Opcode::StoreVariable:     return "StoreVariable";
+        case Opcode::StoreVariableExt:  return "StoreVariableExt";
+        case Opcode::Subtract:          return "Subtract";
+        case Opcode::True:              return "True";
+        default:
+          SH_UNREACHABLE;
+          return "unreachable";
+      }
+    };
+    return fmt::formatter<std::string_view>::format(repr(value), ctx);
+  }
+};
