e43c74dc6aed08bdad03320d9bc7e8d236f1da8b
jsmolka <jsmolka@pm.me>
2022-03-17 21:40:05 +0100

    add get and set expression parser tests

--- tests/src/tests/parser/expr/get.h
+++ tests/src/tests/parser/expr/get.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include "utils.h"
+
+namespace tests_parser_expr_get {
+
+inline suite _ = [] {
+  "parser_expr_get"_test = [] {
+    {
+      constexpr auto kSource = R"(x.y)";
+      constexpr auto kExpect = R"(program
+  expression_statement
+    get y
+      variable x)";
+      parse(kSource, kExpect);
+    }
+    {
+      constexpr auto kSource = R"(x.y.z)";
+      constexpr auto kExpect = R"(program
+  expression_statement
+    get z
+      get y
+        variable x)";
+      parse(kSource, kExpect);
+    }
+    {
+      constexpr const char* kSources[] = {
+        R"(x.)",
+        R"(x."test")",
+        R"(x.0)",
+      };
+
+      for (const auto& source : kSources) {
+        parseThrows(source);
+      }
+    }
+  };
+};
+
+}  // namespace tests_parser_expr_get
--- tests/src/tests/parser/expr/set.h
+++ tests/src/tests/parser/expr/set.h
@@ -1,1 +1,55 @@
 #pragma once
+
+#include "utils.h"
+
+namespace tests_parser_expr_set {
+
+inline suite _ = [] {
+  "parser_expr_set"_test = [] {
+    {
+      constexpr auto kSource = R"(x.y = 0)";
+      constexpr auto kExpect = R"(program
+  expression_statement
+    set y
+      variable x
+      literal 0)";
+      parse(kSource, kExpect);
+    }
+    {
+      constexpr auto kSource = R"(x.y.z = 0)";
+      constexpr auto kExpect = R"(program
+  expression_statement
+    set z
+      get y
+        variable x
+      literal 0)";
+      parse(kSource, kExpect);
+    }
+    {
+      constexpr auto kSource = R"(a + (x.y.z = 0))";
+      constexpr auto kExpect = R"(program
+  expression_statement
+    binary +
+      variable a
+      set z
+        get y
+          variable x
+        literal 0)";
+      parse(kSource, kExpect);
+    }
+    {
+      constexpr const char* kSources[] = {
+        R"(x. = 0)",
+        R"(x."test" = 0)",
+        R"(x.0 = 0)",
+        R"(a + x.y.z = 0)",
+      };
+
+      for (const auto& source : kSources) {
+        parseThrows(source);
+      }
+    }
+  };
+};
+
+}  // namespace tests_parser_expr_set
