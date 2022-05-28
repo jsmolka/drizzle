ab4d2a70359abd44d7cd6b1cf2ce0629738f59c5
jsmolka <jsmolka@pm.me>
2022-05-28 22:23:44 +0200

    add for and iterator tests

--- tests/src/tests/vm/for.h
+++ tests/src/tests/vm/for.h
@@ -16,23 +16,98 @@ for v in [0, 1, 2]:
 assert(i == 3)
 )",
 R"(
-var l = [0, 1, 2]
-var ll = []
-for v in l:
-  ll.push(v)
-  ll.push(l.pop())
-assert(ll == [0, 2, 1, 1])
-)",
-R"(
 var l = []
 for c in "test":
   l.push(c)
 assert(l == ["t", "e", "s", "t"])
 )",
+R"(
+var l = [0, 1, 2]
+var r = []
+for v in l:
+  r.push(v)
+  r.push(l.pop())
+assert(r == [0, 2, 1, 1])
+)",
+R"(
+var l = [0, 1, 2, 3]
+var r = []
+for v in l:
+  if v % 2 != 0:
+    continue
+  r.push(v)
+assert(r == [0, 2])
+)",
+R"(
+var l = [0, 1, 2, 3]
+var i = 0
+for v in l:
+  if v < 2:
+    l.push(4)
+  i = i + 1
+assert(l == [0, 1, 2, 3, 4, 4])
+assert(i == 6)
+)",
+R"(
+var i = forward([1, 2, 3])
+for v in i:
+  assert(v == 1)
+  break
+for v in i:
+  assert(v == 2)
+  break
+for v in i:
+  assert(v == 3)
+  break
+for v in i:
+  assert(false)
+)",
+R"(
+var i = reverse([1, 2, 3])
+for v in i:
+  assert(v == 3)
+  break
+for v in i:
+  assert(v == 2)
+  break
+for v in i:
+  assert(v == 1)
+  break
+for v in i:
+  assert(false)
+)",
+R"(
+var i = 0
+for v in []:
+  i = i + 1
+assert(i == 0)
+)",
+R"(
+var i = 0
+for v in forward([1, 2, 3]):
+  i = i + 1
+assert(i == 3)
+)",
+R"(
+var i = 0
+for v in forward(forward([1, 2, 3])):
+  i = i + 1
+assert(i == 3)
+)",
       };
 
       for (const auto& source : kSources) {
         run(source);
+      }
+    }
+    {
+      constexpr const char* kSources[] = {
+R"(forward(1))",
+R"(reverse(forward([])))",
+      };
+
+      for (const auto& source : kSources) {
+        runThrows(source);
       }
     }
   };
