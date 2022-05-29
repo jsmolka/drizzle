7f6693cc48c080b8aa9e7363480a2de7fe09700e
jsmolka <jsmolka@pm.me>
2022-05-29 21:26:15 +0200

    add map tests

---
+++ tests/src/tests/vm/map.h
@@ -0,0 +1,91 @@
+#pragma
+
+#include "utils.h"
+
+namespace tests_vm_map {
+
+inline suite _ = [] {
+  "vm_map"_test = [] {
+    {
+      constexpr const char* kSources[] = {
+R"(
+var m = {"test": 1}
+assert(m["test"] == 1)
+assert(m.get("test") == 1)
+assert("test" in m)
+assert(m.has("test"))
+)",
+R"(
+var m = {}
+m.set("test", 1)
+assert(m["test"] == 1)
+assert(m.get("test") == 1)
+assert("test" in m)
+assert(m.has("test"))
+)",
+R"(
+var m = {}
+m["test"] = 1
+assert(m["test"] == 1)
+assert(m.get("test") == 1)
+assert("test" in m)
+assert(m.has("test"))
+)",
+R"(
+var m = {"test": 2, "test": 1}
+assert(m["test"] == 1)
+assert(m.get("test") == 1)
+assert("test" in m)
+assert(m.has("test"))
+)",
+R"(
+var m = {"test1": 1, "test2": 2}
+assert(m.size() == 2)
+m.clear()
+assert(m.size() == 0)
+)",
+      };
+
+      for (const auto& source : kSources) {
+        run(source);
+      }
+    }
+    {
+      constexpr const char* kSources[] = {
+R"(
+var m = {1: 1}
+)",
+R"(
+var m = {"test": 1}
+m.get(1)
+)",
+R"(
+var m = {"test": 1}
+m.has(1)
+)",
+R"(
+var m = {"test": 1}
+m.set(1, 1)
+)",
+R"(
+var m = {"test": 1}
+m[1]
+)",
+R"(
+var m = {"test": 1}
+m[1] = 1
+)",
+R"(
+var m = {"test": 1}
+1 in m
+)",
+      };
+
+      for (const auto& source : kSources) {
+        runThrows(source);
+      }
+    }
+  };
+};
+
+}  // namespace tests_vm_map
