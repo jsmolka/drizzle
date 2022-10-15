6f665a47f986fa84f581df71d75f1a93da45d1be
jsmolka <jsmolka@pm.me>
2022-10-15 09:14:25 +0200

    revert function changes

--- drizzle/src/vm.h
+++ drizzle/src/vm.h
@@ -113,6 +113,14 @@ public:  // Todo: ugly
   void range();
   void return_();
   void set();
+  template<std::integral Integral>
+  void store();
+  template<std::integral Integral>
+  void storeGlobal();
+  void subscriptGet();
+  void subscriptSet();
+  void subtract();
+  void true_();
 
   Gc& gc;
   Arguments arguments;
